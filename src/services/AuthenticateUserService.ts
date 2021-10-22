98626745f9c193ef0cfcf875d83248b953b7c3d3
Gustavo Talano <gustavotalano@hotmail.com>
2021-10-22 08:58:44 -0300

    refactor: adjusts token into authentication user service

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
-
+import prismaClient from '../prisma';
+import { sign } from 'jsonwebtoken';
 interface IAccessTokenResponse {
   access_token: string,
 }
@@ -26,13 +27,47 @@ class AuthenticateUserService {
       }
     });
 
-    const response = await axios.get<IAccessTokenResponse>('https://api.github.com/user', {
+    const response = await axios.get<IUserResponse>('https://api.github.com/user', {
       headers: {
         authorization: `Bearer ${AccessTokenResponse.access_token}`
       }
     });
 
-    return response.data;
+    const { login, id, avatar_url, name } = response.data;
+
+    let user = await prismaClient.user.findFirst({
+      where: {
+        github_id: id
+      }
+    });
+
+    if (!user) {
+      user = await prismaClient.user.create({
+        data: {
+          github_id: id,
+          login,
+          avatar_url,
+          name
+        }
+      });
+    }
+
+    const token = sign(
+      {
+        user: {
+          name: user.name,
+          avatar_url: user.avatar_url,
+          id: user.id,
+        },
+      },
+      process.env.JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn: "1d",
+      }
+    );
+
+    return { token, user };
   }
 }
 
