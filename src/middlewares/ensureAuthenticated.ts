e19d8ef4edb629d8ab2fc8c56bf2373ff8e27aad
Gustavo Talano <gustavotalano@hotmail.com>
2022-02-09 21:51:18 -0300

    fix: adds some corrections into middleware

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,33 +1,33 @@
-// import { Request, Response, NextFunction } from 'express';
-// import { verify } from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
 
-// interface IPayload {
-//   sub: string
-// }
+interface IPayload {
+  sub: string
+}
 
-// export function ensureAuthenticated(
-//   request: Request,
-//   response: Response,
-//   next: NextFunction) {
-//   const authToken = request.headers.authorization;
+export function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction) {
+  const authToken = request.headers.authorization;
 
-//   if (!authToken) {
-//     return response.status(401).json({
-//       errorCode: 'token.invalid'
-//     });
-//   }
+  if (!authToken) {
+    return response.status(401).json({
+      errorCode: 'token.invalid'
+    });
+  }
 
-//   const [, token] = authToken.split(' ');
+  const [, token] = authToken.split(' ');
 
-//   try {
-//     const { sub } = verify(token, process.env.JWT_SECRET) as IPayload;
+  try {
+    const { sub } = verify(token, process.env.JWT_SECRET) as IPayload;
 
-//     request.user_id = sub;
+    request.user_id = sub;
 
-//     return next();
-//   } catch (error) {
-//     return response.status(401).json({
-//       errorCode: 'token.expired'
-//     });
-//   }
-// }
+    return next();
+  } catch (error) {
+    return response.status(401).json({
+      errorCode: 'token.expired'
+    });
+  }
+}
